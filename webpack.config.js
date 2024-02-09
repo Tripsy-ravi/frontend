b07dc689ada1928c4b05d0b6b7722f8d6a940fb2
Tripsy-Ravi <tripathiravi207@gmail.com>
2024-02-09 13:06:24 +0530

    .

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+import path from 'path';
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+};

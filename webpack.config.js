2f1f59d08db3676b32c1e88615e245af3bce9ffa
Ravikesh <ravivkn4325@gmail.com>
2024-02-09 14:50:45 +0530

    changes commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,32 @@
-import path from 'path';
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                },
-            },
-            {
-                test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader'],
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.js', '.jsx'],
-    },
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: './public/index.html',
+    })
+  ],
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
 };

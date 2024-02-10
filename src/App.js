c2b765ac538bf32ba7333bdfd70d5cad34eb7655
Ravikesh <ravivkn4325@gmail.com>
2024-02-10 16:40:44 +0530

    files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import DataFetcher from './DataFetcher';
 
+import './index.css';
+
 const App = () => {
   return (
-    <div>
+    <div className='container'>
+      <div className='content'>
       <h1>Fetch Data</h1>
       <DataFetcher />
+      </div>
     </div>
   );
 };
